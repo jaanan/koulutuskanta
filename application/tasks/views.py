72dda9e3b6e060bff555bb21e7acccd07032d1e0
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-13 19:06:15 +0300

    Update views.py
    Määritelty tehtävien lisäämiseen ja muokkaamiseen vaatimus kirjautumisesta.

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_required
 
 from application import app, db
 from application.tasks.models import Task
@@ -9,10 +10,12 @@ def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
 @app.route("/tasks/new/")
+@login_required
 def tasks_form():
     return render_template("tasks/new.html", form = TaskForm())
   
 @app.route("/tasks/<task_id>/", methods=["POST"])
+@login_required
 def tasks_set_done(task_id):
 
     t = Task.query.get(task_id)
@@ -22,6 +25,7 @@ def tasks_set_done(task_id):
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/", methods=["POST"])
+@login_required
 def tasks_create():
     form = TaskForm(request.form)
     
