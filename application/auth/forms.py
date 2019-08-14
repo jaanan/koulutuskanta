bb0e469aacfee05c813003d94a6e35ac61bfbbcf
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-14 10:25:22 +0300

    Update forms.py
    Muutettu tota registration formia aika paljon.

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -8,13 +8,7 @@ class LoginForm(FlaskForm):
     class Meta:
         csrf = False
 
-        from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Name', [validators.Length(min=4, max=25)])
-    email = StringField('Username', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    username = StringField("Name")
+    email = StringField("Username")
+    password = PasswordField("Password")
