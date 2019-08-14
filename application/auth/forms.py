8bc5987efc1cd32c2bba6581573984ea73b08a68
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-14 09:49:56 +0300

    Update forms.py
    Luotu registration form

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -7,3 +7,14 @@ class LoginForm(FlaskForm):
   
     class Meta:
         csrf = False
+
+        from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+class RegistrationForm(Form):
+    username = StringField('Name', [validators.Length(min=4, max=25)])
+    email = StringField('Username', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
