f7214ca90e3b6fa33ea5b43419a3d468341a3871
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-14 15:40:37 +0300

    Update models.py
    Muokattu tiedostoa siten, että jokaiseen käyttäjään liitetään käyttäjän tehtävät.

--- application/auth/models.py
+++ application/auth/models.py
@@ -12,6 +12,8 @@ class User(db.Model):
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
+    
+    tasks = db.relationship("Task", backref='account', lazy=True)
 
     def __init__(self, name, username, password):
         self.name = name
