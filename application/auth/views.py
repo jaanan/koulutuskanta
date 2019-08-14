f7fa58fe70fb2161262c1a13dabae62586dd6099
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-14 10:37:47 +0300

    Update views.py
    lisätty from application.auth.forms import RegistrationForm

--- application/auth/views.py
+++ application/auth/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user, logout_user
 from application import app, db
 from application.auth.models import User
 from application.auth.forms import RegistrationForm
+from application.auth.forms import LoginForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
