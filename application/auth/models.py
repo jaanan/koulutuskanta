5c405db22cd7d45a4425e12e116e0feaf8deca53
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-15 13:27:44 +0300

    Update models.py
    Luokka perii luokan Base

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,28 +1,22 @@
 from application import db
+from application.models import Base
 
-class User(db.Model):
+class User(Base):
 
     __tablename__ = "account"
-  
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
-    
-    tasks = db.relationship("Task", backref='account', lazy=True)
 
-    def __init__(self, name, username, password):
-        self.name = name
-        self.username = username
-        self.password = password
+    tasks = db.relationship("Task", backref='account', lazy=True)
   
+    def __init__(self, name):
+        self.name = name
+
     def get_id(self):
         return self.id
-
+  
     def is_active(self):
         return True
 
