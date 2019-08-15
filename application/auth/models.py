1f73312c197ab5ccda2cf813053c2c53f2899b76
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-15 13:48:17 +0300

    Update models.py
    Lisätty metodi yhteenvetokyselyä varten.

--- application/auth/models.py
+++ application/auth/models.py
@@ -25,3 +25,19 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+      
+    @staticmethod
+    def find_users_with_no_tasks():
+        stmt = text("SELECT Account.id, Account.name FROM Account"
+                     " LEFT JOIN Task ON Task.account_id = Account.id"
+                     " WHERE (Task.done IS null OR Task.done = 1)"
+                     " GROUP BY Account.id"
+                     " HAVING COUNT(Task.id) = 0")
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
+
+        return response
