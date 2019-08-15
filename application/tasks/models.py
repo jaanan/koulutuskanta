8d5d465df1bf4a37fec52b91098aabdd398a2260
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-15 13:23:37 +0300

    Update models.py
    Luokka perii luokan Base

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -1,14 +1,11 @@
 from application import db
+from application.models import Base
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Task(Base):
 
     name = db.Column(db.String(144), nullable=False)
     done = db.Column(db.Boolean, nullable=False)
-    
+
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
     def __init__(self, name):
