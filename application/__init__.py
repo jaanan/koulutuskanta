380700212f5f3b3b47d43a9e74241474f0ddac8e
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-16 15:00:26 +0300

    Update __init__.py
    Muutettu False Trueks

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 from flask_sqlalchemy import SQLAlchemy
 
