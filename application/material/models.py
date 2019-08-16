02e5f5961eed7cea58f1b3a2a79157b818e2e6ca
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-16 14:39:21 +0300

    Update models.py
    Kommentoitu tuo relationship rivi ulos

--- application/material/models.py
+++ application/material/models.py
@@ -5,7 +5,7 @@ class Material(Base):
 
     name = db.Column(db.String(144), nullable=False)
 
-    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
+#    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
 
     def __init__(self, name):
         self.name = name
