113300711caaa84fdba1f25ab09c007eae9a3580
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-16 12:26:10 +0300

    Update views.py
    Muutettu No such username or password muotoon Käyttäjää ei löydy tai salasana on virheellinen.

--- application/auth/views.py
+++ application/auth/views.py
@@ -17,7 +17,7 @@ def auth_login():
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                               error = "No such username or password")
+                               error = "Käyttäjää ei löydy tai salasana on virheellinen.")
 
 
     login_user(user)
