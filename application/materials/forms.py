c1ce39a5450f02ef86ed9acf6ab8531f7cab2feb
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-21 18:37:30 +0300

    Update forms.py
    Lisätty turvautuminen cross-site request forgery -hyökkäyksiä vastaan

--- application/materials/forms.py
+++ application/materials/forms.py
@@ -3,3 +3,6 @@ from wtforms import StringField, validators
 
 class MaterialForm(FlaskForm):
     name = StringField("Materiaalin nimi", [validators.Length(min=8)])
+    
+    class Meta:
+        csrf = False
