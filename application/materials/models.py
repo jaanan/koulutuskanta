878b5f8766db4ded8bda814aac815e823d6052a5
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-21 19:36:18 +0300

    Update models.py
    poistettiin täs vaihees koko foreign key

--- application/materials/models.py
+++ application/materials/models.py
@@ -6,8 +6,6 @@ from sqlalchemy.sql import text
 class Material(Base):
 
     name = db.Column(db.String(144), nullable=False)
-    t = 1
-    task_id = db.Column(db.Integer, db.ForeignKey(t), nullable=False)
 
     def __init__(self, name):
         self.name = name
