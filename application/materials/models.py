04ccc47d4bdb8cdab443d53fd8da97d8dcfdf675
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-21 19:30:38 +0300

    Update models.py
    Kokeillaan task_id tilalla 1:stä

--- application/materials/models.py
+++ application/materials/models.py
@@ -7,7 +7,7 @@ class Material(Base):
 
     name = db.Column(db.String(144), nullable=False)
 
-#    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey(1), nullable=False)
 
     def __init__(self, name):
         self.name = name
