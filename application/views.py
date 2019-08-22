505f402ce57f1f68643d9a87faf5179274c0105a
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-22 21:07:24 +0300

    Update views.py
    lisätty lisää näytettävää etusivulle.

--- application/views.py
+++ application/views.py
@@ -2,7 +2,9 @@ from flask import render_template
 from application import app
 from application.auth.models import User
 from application.tasks.models import Task
+from application.materials.models import Material
 
 @app.route("/")
 def index():
-    return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), all_users=User.find_users(), all_tasks=Task.find_tasks())
+    return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), all_users=User.find_users(), 
+                           all_tasks=Task.find_tasks(), all_materials=Material.find_tasks())
