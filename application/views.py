00e1e04c00267df6fb9b24dc0d79961f8f51aca8
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-22 21:10:29 +0300

    Update views.py
    muutettu yhtä kutsua

--- application/views.py
+++ application/views.py
@@ -7,4 +7,4 @@ from application.materials.models import Material
 @app.route("/")
 def index():
     return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), all_users=User.find_users(), 
-                           all_tasks=Task.find_tasks(), all_materials=Material.find_tasks())
+                           all_tasks=Task.find_tasks(), all_materials=Material.find_materials())
