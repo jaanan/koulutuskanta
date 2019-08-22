72c11354b928c4a3666d7647baed789c0d7d63f1
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-22 20:57:31 +0300

    Update views.py
    muutettu auth tasks

--- application/views.py
+++ application/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from application import app
 from application.auth.models import User
-from application.auth.models import Task
+from application.tasks.models import Task
 
 @app.route("/")
 def index():
