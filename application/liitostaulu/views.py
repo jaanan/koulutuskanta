6a135036fea75afcf937119d63d3ce78bb974109
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-23 20:35:24 +0300

    Update views.py
    koitetaan saada liitostaulu forminäkymään.

---
+++ application/liitostaulu/views.py
@@ -0,0 +1,37 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+
+from application import app, db
+from application.auth.models import User
+from application.liitostaulu.forms import UniteForm
+
+@app.route("/liitostaulu/liitostauluform.html", methods=["GET", "POST"])
+@login_required
+def unite_create():
+    form = UniteForm(request.form)
+    
+    if not form.validate():
+       return render_template("tasks/new.html", form = form)    
+
+    p = Task(form.task.data)
+    m = Task.query.filter(Task.name == form.task.data).count()
+    
+    if m == 0:
+        p.account_id = current_user.id
+
+        db.session().add(p)
+        db.session().commit()
+
+    c = Material(form.material.data)
+    
+    m = Material.query.filter(Material.name == form.material.data).count()
+    
+    if m == 0:
+        db.session().add(c)
+        db.session().commit()
+        
+    p.material2task.append(c)
+    db.session.add(p)
+    db.session.commit()        
+  
+    return redirect(url_for("tasks_index"))
