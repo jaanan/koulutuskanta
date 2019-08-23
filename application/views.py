5851dd2c8518b9c1c4dc1dc0531444cf78c4efed
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-23 19:34:17 +0300

    Update views.py
    kokeillaan vielä

--- application/views.py
+++ application/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask_login import login_required, current_user
+from flask import render_template, request, redirect, url_for
 from application import app
 from application.auth.models import User
 from application.tasks.models import Task
