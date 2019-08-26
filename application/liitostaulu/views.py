f0dc8302fde699bec85867f1ea03a21697d734f5
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-26 21:08:17 +0300

    Update views.py
    muokattu

--- application/liitostaulu/views.py
+++ application/liitostaulu/views.py
@@ -34,7 +34,7 @@ def unite_create():
     materiaali_id = materiaali.id
         
     conn = db.session.connection()
-    ins = assoc.insert().values(book_id=koulutus_id,tag_id=materiaali_id)
+    ins = koulutusmateriaali.insert().values(book_id=koulutus_id,tag_id=materiaali_id)
     result = conn.execute(ins)
     db.session.commit()       
   
