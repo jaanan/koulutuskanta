1e2ffc6d8f9e7cd41215a517e23360b18164c55c
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-26 19:44:38 +0300

    Update models.py
    muutettu taas tuota liitostauluosioo.

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -11,7 +11,7 @@ class Task(Base):
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
     #Relationships
-    materials = db.relationship("Material", secondary = material2task)
+    taskmaterials = db.relationship('Material', secondary=material2task, backref=db.backref('koulutusmateriaalit', lazy = 'dynamic'))
 
     def __init__(self, name):
         self.name = name
