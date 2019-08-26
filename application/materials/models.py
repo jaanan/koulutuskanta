ad4fa4fc13e2147c66ba2335c9d5947389fee06b
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-26 18:51:09 +0300

    Update models.py
    Muokattu relationshipiä

--- application/materials/models.py
+++ application/materials/models.py
@@ -6,9 +6,6 @@ from sqlalchemy.sql import text
 class Material(Base):
 
     name = db.Column(db.String(144), nullable=False)
-    
-    material2task = db.relationship("Task", secondary = material2task,
-                               backref=db.backref('materials', lazy = 'dynamic'))
 
     def __init__(self, name):
         self.name = name
