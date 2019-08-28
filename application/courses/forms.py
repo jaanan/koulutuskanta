5441ea5ba42f120d1ba5733e666f56ddd5230b13
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-28 18:40:53 +0300

    Update forms.py
    Lisätty CourseForm

---
+++ application/courses/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
+
+class CourseForm(FlaskForm):
+    name = StringField("Kurssin nimi", [validators.Length(min=8)])
+    
+    class Meta:
+        csrf = False
