d3439d457c4fe0ef662403d0ba25a59a5fff6e7a
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-28 19:10:56 +0300

    Update forms.py
    lisätty

---
+++ application/kurssimateriaalitaulu/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, validators
+
+class UnionForm(FlaskForm):
+    course = StringField("Kurssin nimi", [validators.Length(min=8)])
+    material = StringField("Materiaalin nimi", [validators.Length(min=8)])
+
+    class Meta:
+        csrf = False
