7e0a1c39a2553202f4ed97dc7bd0fac75a0e17db
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-28 21:13:06 +0300

    Update views.py
    lisätty course_materials=Course.courseto_material()

--- application/views.py
+++ application/views.py
@@ -10,4 +10,4 @@ def index():
     return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), all_users=User.find_users(), 
                            all_tasks=Task.find_tasks(), all_materials=Material.find_materials(), 
                            all_taskmaterials=Task.connect_material(), task_users=Task.connect_account(),
-                           users_taskmaterials=Task.connect_taskmaterial())
+                           users_taskmaterials=Task.connect_taskmaterial(), course_materials=Course.courseto_material())
