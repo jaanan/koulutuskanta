990b76a83e9ca7fa0ddb5d9e4e1890c98c440cd9
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-29 19:04:52 +0300

    Update models.py
    Poistettu AND

--- application/auth/models.py
+++ application/auth/models.py
@@ -62,8 +62,7 @@ class User(Base):
     @staticmethod
     def studentto_course():
         stmt = text("SELECT Account.name AS Työntekijä, Course.name AS Kurssi FROM Account, Course, kurssilainen"
-                        " WHERE Account.id = kurssilainen.'account.id'"
-                            " AND Course.id = kurssilainen.'course.id';")
+                        " WHERE Account.id = kurssilainen.'account.id'";")
         
         result = db.engine.execute(stmt)
         ids = []
