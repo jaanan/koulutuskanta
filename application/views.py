bd137fcd76abc3f1ac051069b5f6fbc24e103e00
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-29 20:40:55 +0300

    Update views.py
    lisätty login required

--- application/views.py
+++ application/views.py
@@ -4,7 +4,9 @@ from application.auth.models import User
 from application.tasks.models import Task
 from application.materials.models import Material
 from application.courses.models import Course
+from flask_login import login_required, current_user
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html", all_users=User.find_users(), all_materials=Material.find_materials(), course_students=User.studentto_course())
