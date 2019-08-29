188d181012f6a11a91fab0bcd06cda06aa06d79a
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-29 16:43:49 +0300

    Update views.py
    poistettu needs_tasks=User.find_users_with_no_tasks() all tasks
    
                               all_taskmaterials=Task.connect_material(), task_users=Task.connect_account(),
                               users_taskmaterials=Task.connect_taskmaterial()

--- application/views.py
+++ application/views.py
@@ -7,7 +7,4 @@ from application.courses.models import Course
 
 @app.route("/")
 def index():
-    return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), all_users=User.find_users(), 
-                           all_tasks=Task.find_tasks(), all_materials=Material.find_materials(), 
-                           all_taskmaterials=Task.connect_material(), task_users=Task.connect_account(),
-                           users_taskmaterials=Task.connect_taskmaterial())
+    return render_template("index.html", all_users=User.find_users(), all_materials=Material.find_materials())
