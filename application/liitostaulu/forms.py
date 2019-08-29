86f0fc7630863bcde0af21deae716a32416a23a9
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-29 17:04:10 +0300

    Delete forms.py
    poistetaan turha formi

--- application/liitostaulu/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, validators
-
-class UniteForm(FlaskForm):
-    task = StringField("Koulutuksen nimi", [validators.Length(min=8)])
-    material = StringField("Materiaalin nimi", [validators.Length(min=8)])
-
-    class Meta:
-        csrf = False
