637b79f8405f371dba4edb9bf1c05d135fad8f96
jaanan <42993030+jaanan@users.noreply.github.com>
2019-08-30 19:41:14 +0300

    Update models.py
    lisätty luokka role

---
+++ application/roles/models.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import current_user, login_required, RoleMixin, Security, \
+    SQLAlchemyUserDatastore, UserMixin, utils
+from flask_mail import Mail
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib import sqla
+
+from wtforms.fields import PasswordField
+
+class Role(db.Model, RoleMixin):
+
+    # Our Role has three fields, ID, name and description
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    # __str__ is required by Flask-Admin, so we can have human-readable values for the Role when editing a User.
+    # If we were using Python 2.7, this would be __unicode__ instead.
+    def __str__(self):
+        return self.name
+
+    # __hash__ is required to avoid the exception TypeError: unhashable type: 'Role' when saving a User
+    def __hash__(self):
+        return hash(self.name)
