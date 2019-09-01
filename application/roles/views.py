19d9e677a873bff36882aea86c645ba5336a7b4f
jaanan <42993030+jaanan@users.noreply.github.com>
2019-09-01 17:24:13 +0300

    Update views.py
    lisätty from flask_login import login_required, current_user

--- application/roles/views.py
+++ application/roles/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
+from flask_login import login_required, current_user
 
 from application import app, db
 from application.auth.models import User
