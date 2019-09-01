68d6ab31ef4735a59d4f120a8c653753ff5c822d
jaanan <42993030+jaanan@users.noreply.github.com>
2019-09-01 23:38:17 +0300

    Update views.py
    kokeillaan toimiiko tämä

--- application/materials/views.py
+++ application/materials/views.py
@@ -35,4 +35,4 @@ def materials_create():
         db.session().add(t)
         db.session().commit()
   
-    return redirect(url_for("material_index"))
+    return redirect(url_for("/materials/new/"))
