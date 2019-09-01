7e0dffd0adb017393662bdee3d0283b83df26e1b
jaanan <42993030+jaanan@users.noreply.github.com>
2019-09-01 17:27:41 +0300

    Update views.py
    poistettu s

--- application/roles/views.py
+++ application/roles/views.py
@@ -12,12 +12,12 @@ from sqlalchemy.sql import text
 @app.route("/roles", methods=["GET"])
 @login_required
 def roles_index():
-    return render_template("roles/rolesform.html", roles = Role.query.all())
+    return render_template("roles/roleform.html", roles = Role.query.all())
 
 @app.route("/roles/new/")
 @login_required
 def roles_form():
-    return render_template("roles/rolesform.html", form = RoleForm(), roles = Role.query.all())
+    return render_template("roles/roleform.html", form = RoleForm(), roles = Role.query.all())
 
 
 
@@ -27,11 +27,11 @@ def roles_create():
     if request.method == "POST":
         form = RoleForm(request.form)
         if User.query.filter(User.name == form.name.data).count() > 0:
-            return render_template("roles/rolesform.html", form = RoleForm())
+            return render_template("roles/roleform.html", form = RoleForm())
         else:
             user = User(form.name.data)
             role = Role(form.role.data)
             db.session().add(user)
             db.session().commit()
             return render_template("roles/rolesform.html", form = RoleForm())
-    return render_template("roles/rolesform.html", form = RoleForm())
+    return render_template("roles/roleform.html", form = RoleForm())
