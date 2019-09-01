0cabae8dfc8e0570561b31d2a8ee46f23765d701
jaanan <42993030+jaanan@users.noreply.github.com>
2019-09-01 19:54:12 +0300

    Update models.py
    kokkeillaan

--- application/roles/models.py
+++ application/roles/models.py
@@ -22,6 +22,16 @@ class Role(Base, RoleMixin):
     def __hash__(self):
         return hash(self.name)
     
+    def required_roles(*roles):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if is_accessible() not in roles:
+                flash('Authentication error, please check your details and try again','error')
+                return redirect(url_for('index'))
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
  
     @staticmethod
     def is_accessible():
