b5f76533b25de959e8084c67c1ba570d39356a71
jaanan <42993030+jaanan@users.noreply.github.com>
2019-09-01 20:59:10 +0300

    Update views.py
    from flask import flash

--- application/roles/views.py
+++ application/roles/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 from flask_login import login_required, current_user
 from application.models import Base, roles_users
+from flask import flash
 
 from application import app, db
 from application.auth.models import User
