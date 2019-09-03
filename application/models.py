aa3e3694d2e52da5a1facf305979a9a2b55828d4
Jaana Laitinen <jaana.m.laitinen@helsinki.fi>
2019-09-03 12:40:44 +0300

    muutettiin takas pisteet noihin.

--- application/models.py
+++ application/models.py
@@ -11,16 +11,16 @@ class Base(db.Model):
 
 
 kurssimateriaali = db.Table('kurssimateriaali',
-                          db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-                          db.Column('material_id', db.Integer, db.ForeignKey('material.id')),
-                          db.PrimaryKeyConstraint('course_id', 'material_id'))
+                          db.Column('course.id', db.Integer, db.ForeignKey('course.id')),
+                          db.Column('material.id', db.Integer, db.ForeignKey('material.id')),
+                          db.PrimaryKeyConstraint('course.id', 'material.id'))
 
 kurssilainen = db.Table('kurssilainen',
-                          db.Column('account_id', db.Integer, db.ForeignKey('account.id')),
-                          db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-                          db.PrimaryKeyConstraint('account_id', 'course_id'))
+                          db.Column('account.id', db.Integer, db.ForeignKey('account.id')),
+                          db.Column('course.id', db.Integer, db.ForeignKey('course.id')),
+                          db.PrimaryKeyConstraint('account.id', 'course_id'))
 roles_users = db.Table('roles_users',
-                          db.Column('account_id', db.Integer, db.ForeignKey('account.id')),
-                          db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
-                          db.PrimaryKeyConstraint('account_id', 'role_id'))
+                          db.Column('account.id', db.Integer, db.ForeignKey('account.id')),
+                          db.Column('role.id', db.Integer, db.ForeignKey('role.id')),
+                          db.PrimaryKeyConstraint('account.id', 'role.id'))
 
