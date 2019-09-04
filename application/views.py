223a264c0609153e2df8f01eaad21391745baad7
Jaana Laitinen <jaana.m.laitinen@helsinki.fi>
2019-09-04 20:48:15 +0300

    importattu required roles authista

--- application/views.py
+++ application/views.py
@@ -5,9 +5,11 @@ from application.tasks.models import Task
 from application.materials.models import Material
 from application.courses.models import Course
 from flask_login import login_required, current_user
+from application.auth.models import required_roles
 
 @app.route("/")
 @login_required
+@required_roles('admin')
 def index():
     return render_template("index.html", all_users=User.find_users(), course_students=User.studentto_course(), 
                            student_materials=User.find_materials_and_users(), find_everything=User.find_materials_courses_users(),
