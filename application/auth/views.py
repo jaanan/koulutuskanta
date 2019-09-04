5841d21639cf1179217179ce6a0e1b2e2589c45e
Jaana Laitinen <jaana.m.laitinen@helsinki.fi>
2019-09-04 17:55:11 +0300

    lisätty import login required

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
+from flask_security import current_user, login_required, RoleMixin, Security, \
+    SQLAlchemyUserDatastore, UserMixin, utils
 
 from application import app, db
 from application.auth.models import User
