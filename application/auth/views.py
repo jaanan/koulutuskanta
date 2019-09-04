2b431e3f90e67eccef5992929269c18a564e79bf
Jaana Laitinen <jaana.m.laitinen@helsinki.fi>
2019-09-05 02:22:52 +0300

    åpostetti frmei importointi

--- application/auth/views.py
+++ application/auth/views.py
@@ -5,7 +5,7 @@ from flask_security import current_user, login_required, RoleMixin, Security, \
 
 from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm, RolesForm, RegistrationForm, NameChangeForm, U_nameChangeForm, PasswordChangeForm
+from application.auth.forms import LoginForm, RolesForm, RegistrationForm
 from flask import flash
 from functools import wraps
 
