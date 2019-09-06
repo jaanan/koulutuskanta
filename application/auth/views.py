e7ac00cde77fb60c134d0bcf1b15d2dbede234fa
Jaana Laitinen <jaana.m.laitinen@helsinki.fi>
2019-09-06 11:00:27 +0300

    toimisko tää

--- application/auth/views.py
+++ application/auth/views.py
@@ -164,7 +164,7 @@ def remove_user():
        return redirect(url_for("personal_space"))  
 
     user_to_be_removed = User.query.filter(User.username == form.username.data).first()
-    d = kurssilainen.delete().where(kurssilainen."account_id" = user_to_be_removed.id)
+    kurssilainen.delete().where(kurssilainen."account_id" = user_to_be_removed.id)
     db.session.commit()
     flash('The account has been removed')
     return redirect(url_for("index"))
